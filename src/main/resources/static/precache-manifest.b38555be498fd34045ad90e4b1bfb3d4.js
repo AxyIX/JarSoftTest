f3342c3e3312370b030be2be038d1753a4863945
AxyIX <alexmauster@mail.ru>
2019-06-04 15:02:03 +0700

    add compiled frontend

---
+++ src/main/resources/static/precache-manifest.b38555be498fd34045ad90e4b1bfb3d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a8332fcbc6aa9e05c92381e14b12f22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b10e8e7d1f58737e5bac",
+    "url": "/static/css/main.7a0b9f7b.chunk.css"
+  },
+  {
+    "revision": "a07412e85b0a2d673238",
+    "url": "/static/js/2.61fc8906.chunk.js"
+  },
+  {
+    "revision": "b10e8e7d1f58737e5bac",
+    "url": "/static/js/main.2d601739.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "876b75bff1eda9cf6e8806f8b56a886f",
+    "url": "/static/media/search.876b75bf.svg"
+  },
+  {
+    "revision": "025e93430a4e57bd6408bd51d2f83cdb",
+    "url": "/static/media/triangle.025e9343.svg"
+  }
+]);
